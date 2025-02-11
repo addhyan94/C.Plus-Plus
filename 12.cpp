846810628814d49c165f0a81545f7a809b73f6e9
addhyan94 <addhyant85@gmail.com>
2025-02-11 13:45:43 +0530

    input a Decinal number .

---
+++ 12.cpp
@@ -0,0 +1,12 @@
+// WAP to input a Decinal number .
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float a;
+    cout << "Enter a number: ";
+    cin >> a;
+    cout << "The number is: " << a;
+    return 0;
+}
