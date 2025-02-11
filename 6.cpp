8f1fea85cb660ad74b9c42c8836fc827c44395c2
addhyan94 <addhyant85@gmail.com>
2025-02-11 12:58:15 +0530

    Print Decimal Number

---
+++ 6.cpp
@@ -0,0 +1,11 @@
+// WAP to print a any Floating type number .
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float a;
+    a=10.5;
+    cout<<"The Floating Type number is: "<<a;
+    return 0;
+}
