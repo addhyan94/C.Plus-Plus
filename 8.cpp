90c16dcc140b93bd7698917e8337f60a92299f9f
addhyan94 <addhyant85@gmail.com>
2025-02-11 12:57:30 +0530

    Print Integer , Decimal, character Types value

---
+++ 8.cpp
@@ -0,0 +1,17 @@
+// WAP to print one integer , one decimal , one character Types value using defrent print function. 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    float b;
+    char c;
+    a=10;
+    b=10.5;
+    c='A';
+    cout<<"The Integer is: "<<a<<endl;
+    cout<<"The Decimal is: "<<b<<endl;
+    cout<<"The Character is: "<<c;
+    return 0;
+}
