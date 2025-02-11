0a9dd6117476ec70e145d126fb37176bc8f140d1
addhyan94 <addhyant85@gmail.com>
2025-02-11 13:45:31 +0530

    input a character Types value.

---
+++ 13.cpp
@@ -0,0 +1,12 @@
+// WAP to input a character Types value.
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char a;
+    cout << "Enter a character: ";
+    cin >> a;
+    cout << "The character is: " << a;
+    return 0;
+}
