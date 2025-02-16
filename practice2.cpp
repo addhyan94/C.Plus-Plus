e0c31ef34bb0f61cd171342f1b7d57e535aaa3c2
addhyan94 <addhyant85@gmail.com>
2025-02-16 18:37:57 +0530

    practice2.cpp to demonstrate constants in C++

---
+++ practice2.cpp
@@ -0,0 +1,13 @@
+//  constants in c++ .
+#include<iostream>
+using namespace std;
+int main()
+{
+    const int a = 20;
+    cout<<"The value of A was :"<<a;
+    // a=30;
+    // ek baar a naam ki variable ko constant declare karne ke baad uski value change nahi kar sakte.
+    cout<<"The value of A is :"<<a;
+    return 0;
+    
+}
