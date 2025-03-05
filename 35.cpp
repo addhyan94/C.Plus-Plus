1f3ecd6dd50ac45a944e03c79fbf8ff22683a727
addhyan94 <addhyant85@gmail.com>
2025-03-05 20:52:07 +0530

    overload unary - operator.

---
+++ 35.cpp
@@ -0,0 +1,38 @@
+// Wap to overload unary - operator.
+#include <iostream>
+using namespace std;
+
+class unary
+{
+    int a;
+    public:
+    void getdata()
+    {
+        cout<<"Enter the value of a=";
+        cin>>a;
+    }
+    void display()
+    {
+        cout<<"\nValue of a="<<a;
+    }
+    void operator -()
+    {
+        a=-a;
+    }
+};
+
+int main()
+{
+    unary u;
+    u.getdata();
+    u.display();
+    -u;
+    u.display();
+    return 0;
+}
+
+/* Output ~
+Enter the value of a=10
+Value of a=10
+Value of a=-10
+*/
