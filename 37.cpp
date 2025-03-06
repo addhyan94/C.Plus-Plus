5e17cd76fe2993ca27981a97fe6ad9af6bbdee24
addhyan94 <addhyant85@gmail.com>
2025-03-06 06:33:13 +0530

    sum two number and print them.

---
+++ 37.cpp
@@ -0,0 +1,12 @@
+//  WAP to sum two number and print them.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"Enter the value of a and b : "<<endl;
+    cin>>a>>b;
+    cout<<"Sum of a ("<<a<<") and b ("<<b<<") is : "<<a+b<<endl;
+    return 1;
+}
